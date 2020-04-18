3e140c0592ccaf96a73244efaf093d8bc3c4a0e8
majung2 <dlalwjd1417@likelion.org>
2020-04-18 18:37:23 +0900

    kakaoMapAPI

--- GMMproject/GMMproject/urls.py
+++ GMMproject/GMMproject/urls.py
@@ -20,4 +20,5 @@ from odso import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home"),
+    path('map/',views.map, name="map"),
 ]
--- GMMproject/odso/views.py
+++ GMMproject/odso/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def map(request):
+    return render(request, 'map.html')
